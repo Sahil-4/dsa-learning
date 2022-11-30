0daa8e68875be53d9c242bc35a82b95951007852
Sahil-4 <www.sahil.875020@gmail.com>
2022-11-30 12:31:42 +0530

    added code for lecture 7

--- lecture7.c
+++ lecture7.c
@@ -5,84 +5,96 @@
 
 void problem1() {
     // Problem : 7. Reverse Integer - https://leetcode.com/problems/reverse-integer/
+
     // Problem statement : Given a signed 32-bit integer x, 
     // return x with its digits reversed. 
     // If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], 
     // then return 0.
-    
-    int n = 123;
+
+    int n = 123; // output : 321 
+    n = 321; // output : 123 
+    n = 655554445; // output : 544455556 
+    n = 544455556; // output : 655554445 
+    n = 1534236469; // output : 0 
+
+    //code here 
     int ans = 0; // for final answer 
-    
+
     // reversing n 
     while (n != 0) {
-        int d = n%10;
-        
         // if reversed goes out of range 
         if ((ans > INT_MAX/10) || (ans < INT_MIN/10)) {
             printf("%d", 0);
-            break;
+            return;
         }
-        
+
+        int d = n%10;
         ans = (ans * 10) + d;
         n = n / 10;
     }
-    
+
     printf("%d", ans);
 }
 
 void problem2() {
-    // Problem : 1009. Complement of Base 10 Integer
+    // Problem : 1009. Complement of Base 10 Integer - https://leetcode.com/problems/complement-of-base-10-integer/ 
+
     // Problem statement : Given an integer n, return its complement.
     // The complement of an integer is the integer you get,
     // when you flip all the 0's to 1's 
     // and all the 1's to 0's in its binary representation.
 
-    int n = 0;
-    
+    int n;
+    n = 0; // out : 1 
+    n = 4; // out : 3 
+    n = 5; // out : 2 
+
     if (n == 0) {
         printf("1");
         return;
     }
 
-    int temp = ~n;
+    int not_of_n = ~n;
     int mask = 0;
 
     while (n != 0) {
         n = n >> 1;
-        mask = mask << 1;
-        mask = mask | 1;
+        mask = mask << 1; // xxxxxxxx x 
+        mask = mask | 1; // xxxxxxxxx 1 
     }
-    
-    printf("%d", temp & mask);
+
+    printf("%d", not_of_n & mask);
 }
 
 void problem3() {
-    // Problem : 231. Power of Two 
+    // Problem : 231. Power of Two - https://leetcode.com/problems/power-of-two/ 
+
     // Problem statement : Given an integer n, 
     // return true if it is a power of two. 
     // Otherwise, return false.
 
-    int n = 0;
+    int n = 32;
     int m = 1;
-    
-    while (n > 1) {
-        if (n == m) {
-            printf("true");
+
+    for (int i = 0; i < 31; i++) {
+        if (m == n) {
+            printf("true\n");
             return;
         }
-
-        n = n >> 1;
         m = m << 1;
     }
-    
+
     printf("false");
 }
 
 int main() {
     // lecture 7 : Leetcode problem solving 
+
     // problem1();
+
     // problem2();
-    problem3();
+
+    // problem3();
 
     return 0;
 }
