44672730da2b6000f1954ec10fb1acdbbf20f82e
Sahil-4 <www.sahil.875020@gmail.com>
2022-12-16 17:34:19 +0530

    added code for lecture 14

--- lecture14.c
+++ lecture14.c
@@ -4,12 +4,13 @@
 
 void p1() {
     // Problem 1 : Find pivot in an sorted and rotated array 
+
     // Statement : given a sorted and rotated array of integers 
     // return the pivot (minimum) element of the array 
 
     // Logic : Using binary search 
-    // take 3 vars low = 0, high = size-1, mid = mid = (low+high)/2
-    // run a loop while (low < high)
+    // take 3 vars low = 0, high = size-1, mid = mid = (low+high)/2 
+    // run a loop while (low < high) 
     // check for below conditions 
     // 1. arr[mid] >= arr[0] 
     // 2. arr[mid] < arr[0] 
@@ -37,7 +38,8 @@ void p1() {
 }
 
 void p2() {
-    // Problem 2 : Search In Rotated Sorted Array - https://www.codingninjas.com/codestudio/problems/search-in-rotated-sorted-array_1082554
+    // Problem 2 : Search In Rotated Sorted Array - https://www.codingninjas.com/codestudio/problems/search-in-rotated-sorted-array_1082554 
+
     // Statement : You have been given a sorted array of ‘N’ elements,
     // and an integer ‘K’ 
     // Now the array is rotated at some pivot point unknown to you 
@@ -46,46 +48,25 @@ void p2() {
     // the array will be ARR = [7, 8, 1, 3, 5] 
     // find the index at which ‘K’ is present in array 
 
-    // int size = 4, arr[4] = {2, 3, 5, 8};
+    // Logic : Using binary search 
+    // first find pivot(smallest element of the array),
+    // then check for below conditions 
+    // 1. arr[0] < K && K < arr[pivot-1]
+    // 2. arr[0] > K && K > arr[pivot]
+    // if 1 find K in first part of the array i = 0 to i < pivot 
+    // if 2 find K in second part of the array i = pivot to i < size_of_array 
+
+    // Code : 
+    int size = 4, arr[4] = {2, 3, 5, 8};
+    int key = 2;
     // int size = 1, arr[1] = {2};
-    int size = 8, arr[8] = {3, 4, 5, 6, 8, 9, 1, 2};
-    int key = 1;
-/*
-    int low = 0, high = size-1;
-    int mid = low + (high-low)/2;
-    while (low <= high) {
-        if (arr[mid] == key) {
-            printf("key : %d found at index : %d\n", arr[mid], mid);
-            break;
-        }
+    // int key = 3;
+    // int size = 8, arr[8] = {3, 4, 5, 6, 8, 9, 1, 2};
+    // int key = 1;
 
-        if (arr[0] <= key && key < arr[mid]) {
-            high = mid-1;
-        } else if (arr[size-1] >= key && key > arr[mid]) {
-            low = mid+1;
-        } else if (arr[0] < key && key > arr[mid]) {
-            low = mid+1;
-        } else if (arr[size-1] >= key && key < arr[mid]) {
-            high = mid-1;
-        } else {
-            break;
-        }
-
-        mid = low + (high-low)/2;
-    }
-
-    printf("key not found\n");
-    // !!!!!!!!!!!!!!!!!!! NOT WORKING !!!!!!!!!!!!!!!!!!! // 
-*/
-
-    // Another solution 
-    // find pivot element 
-    // check for below conditions 
-    // if pivot <= key <= arr[n-1]; true then search in second part on the array 
-    // else search in first part 
-
-    int low = 0, high = size-1;
-    int mid = low + (high-low)/2;
+    int low, high, mid;
+    low = 0, high = size-1;
+    mid = low + (high-low)/2;
 
     // finding pivot 
     while (low < high) {
@@ -101,25 +82,23 @@ void p2() {
     int pivot = low;
 
     // checking conditions 
-    if (arr[pivot] <= key && key <=arr[size-1]) {
-        // search in second part i = pivot to <size
+    if (arr[pivot] <= key && key <= arr[size-1]) {
+        // search in second part i = pivot to i < size 
         low = pivot, high = size-1;
     } else {
-        // search in first part i = 0 to <pivot 
+        // search in first part i = 0 to i < pivot 
         low = 0, high = pivot-1;
     }
 
     // search binary
     mid = low + (high-low)/2;
-    int flag = -1;
 
     while (low <= high) {
         if (arr[mid] == key) {
-            flag = 1;
             printf("key : %d found at index : %d", key, mid);
-            break;
+            return;
         }
-        
+
         if (arr[mid] > key) {
             high = mid-1;
         } else {
@@ -129,15 +108,15 @@ void p2() {
         mid = low + (high-low)/2;
     }
 
-    if (flag == -1) {
-        printf("key not found");
-    }
+    printf("key not found");
 }
 
 void p3() {
     // Problem 3 : find square root of n 
+
     // Statement : given an integer n,
     // return square root of n 
+
     // Logic : using binary search
     // low = 0, high = n 
     // get mid check for below conditions 
@@ -148,13 +127,14 @@ void p3() {
     // if 2 is true save mid as answer and update low = mid+1
     // if 3 is true update high = mid-1
 
+    // Code : 
     int n;
     scanf("%d", &n);
-    
+
     int low = 0, high = n;
     long long int mid = low + (high-low)/2;
     int ans = -1;
-    
+
     while (low <= high) {
         if (mid * mid == n) {
             ans = mid;
@@ -191,11 +171,12 @@ void p3() {
 
 int main() {
     // Lecture 14 : Binary search interview questions 
+
     // p1();
     
     // p2();
 
-    p3();
+    // p3();
 
     return 0;
 }
