1c9ff1f11963f6a085d255a38fe15f0df31eea2b
Sahil-4 <www.sahil.875020@gmail.com>
2022-12-28 17:29:59 +0530

    updated code for lecture 22

--- lecture22.cpp
+++ lecture22.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <stack>
+#include <algorithm>
 
 using namespace std;
 
@@ -18,7 +21,10 @@ void p1() {
     // 2. string : 1D char array 
 
     // string declaration and initialisation 
-    char str[6] = {'H', 'e', 'l', 'l', 'o'};
+    // char str[6] = {'H', 'e', 'l', 'l', 'o'};
+
+    // stl sting 
+    string str;
 
     // taking string input from user 
     cin >> str;
@@ -33,8 +39,15 @@ void p2() {
     // given an array of characters 
     // return the length of string 
 
+    // logic : 
+    // take i = 0
+    // traverse the array of chars 
+    // if arr[i] != 0 
+    // increament count by 1 
+    // else return the count 
+
     char ch[12];
-    
+
     cout << "enter your string : ";
     cin >> ch;
     cout << endl;
@@ -53,6 +66,10 @@ void p3() {
     // reverse the array 
     // then print 
 
+    // logic : 
+    // traverse the whole array from i = 0 to i <= size/2 
+    // replace first element by last element 
+
     char ch[12] = "this"; // output : siht 
 
     int l = 0, h = 3;
@@ -70,6 +87,14 @@ void p4() {
     // ignore all symbols and whitespaces, case sensitive 
     // then check is the string is palindrome or not 
 
+    // logic : 
+    // first traverse the string and remove all symbols and whitespaces in the array 
+    // and make the srtring case in-sensitive 
+    // now traverse the array from i = 0 to i < size/2 
+    // check if arr[i] is == to arr[size-i-1] 
+    // if not true return that string is not palindrome 
+    // else increament i by 1 and go to next element 
+
     // string s = "N2 i&nJA?a& jnI2n";
     string s = "A1b22Ba";
 
@@ -83,6 +108,7 @@ void p4() {
             if (!(s[i] >= 48 && s[i] <= 57)) {
                 int c = s[i];
                 if (c >= 65 && c <= 90) {
+                    // converting to lowercase 
                     char c2 = 122 - (90 - c);
                     s[i] = c2;
                 }
@@ -104,7 +130,7 @@ void p4() {
     while (l < h) {
         if (s[l++] != s[h--]) {
             cout << "not a palindrome";
-            break;
+            return;
         }
     }
     cout << "palindrome";
@@ -116,6 +142,17 @@ void p5() {
     // Statement : Given a string s or phrase, 
     // return true if it is a palindrome, 
     // or false if it is not a palindrome
+    // ignore all whitespaces and chars other than alphanumeric 
+
+    // Logic : using two pointer 
+    // initialise l = 0, h = length of string - 1 
+    // run a loop till l < h 
+    // check for string[l] and string[h] 
+    // if anyone char is not alphanumeric skip that 
+    // else convert them into lowercase 
+    // check if string[l] == string[h] 
+    // if yes go to next indices 
+    // else return not palindrome 
 
     string s = "A man, a plan, a canal: Panama";
     int l = 0, h = s.length()-1;
@@ -135,14 +172,17 @@ void p5() {
             continue;
         }
 
+        // converting to lowercase 
         if (ch_l >= 'A' && ch_l <= 'Z') {
             ch_l = 'a' + (ch_l - 'A');
         }
 
+        // converting to lowercase 
         if (ch_h >= 'A' && ch_h <= 'Z') {
             ch_h = 'a' + (ch_h - 'A');
         }
 
+        // checking for not palindrome 
         if (ch_l != ch_h) {
             cout << "not a palindrome" << endl;
             return;
@@ -162,12 +202,24 @@ void p6() {
     // reverse the order of the words, 
     // word : sequence of non space characters 
 
-    // char s[] = {'t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e'};
-    // int size = 15;
+    // Logic : using Bruteforce 
+    // take an array w_chars to store reverse of given string 
+    // take a variable count = 0 
+    // and i = 0 
+    // run a loop till count < size of string 
+    // check s[count] != " " (whitespace)
+    // if true put s[count] in chars at index i 
+    // now increament i by 1 
+    // else update i = 0 
+    // add a space at the very beginning of chars 
+    // at the end increament count by 1 
+
+    char s[] = {'t', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e'};
+    int size = 15;
     // char s[] = {'s', 'h', 'e', ' ', 'b', ' ', 'a'};
-    // int size = 7;
-    char s[] = {'t', 'h', 'e'};
-    int size = 3;
+    // size = 7;
+    // char s[] = {'t', 'h', 'e'};
+    // size = 3;
 
     vector<char> v_cr;
 
@@ -199,6 +251,14 @@ void p7() {
     // If more than one character occurs the maximum number of time 
     // then print the lexicographically smaller character.
 
+    // Logic : 
+    // take an array freq of size 26 
+    // in that array every element denotes the frequency of char from a to z 
+    // now traverse the given string i = 0 to i < size of string 
+    // get the char at str[i]
+    // and increament frequency of that perticular char 
+    // finally check the maximum occurring element 
+
     string s = "testsample";
     int freq[26] = {0};
 
@@ -228,6 +288,12 @@ void p8() {
     // Statement : You have been given a string 'STR' of words. 
     // You need to replace all the spaces between words with “@40”.
 
+    // logic : 
+    // traverse the string 
+    // find all the whitespaces and replace it with "@40"
+    // to find whitespaces use String.find(character);
+    // to replace use String.replace(position, character_to_replace, new_character);
+
     string str = "Coding Ninjas Is A Coding Platform";
 
     while (str.find(' ') != 0 && str.find(' ') <= str.length()) {
@@ -240,10 +306,15 @@ void p8() {
 void p9() {
     // Problem 8 : 1910. Remove All Occurrences of a Substring - https://leetcode.com/problems/remove-all-occurrences-of-a-substring/ 
 
-    // Statement : Given two strings s and part, 
-    // perform the following operation on s until all occurrences of the substring part are removed:
+    // Statement : Given two strings str and part, 
+    // perform the following operation on str until all occurrences of the substring part are removed:
     // Find the leftmost occurrence of the substring part and remove it from s.
     // Return s after removing all occurrences of part.
+
+    // Logic : 
+    // find part in str 
+    // if found erase that part 
+    // using String.erase(position, length_of_part);
 
     string str = "daabcbaabcbc";
     string part = "abc";
@@ -272,64 +343,80 @@ void p10() {
     // Statement : Given two strings s1 and s2, 
     // return true if s2 contains a permutation of s1, 
     // or false otherwise.
+    // permutation : making a new string, 
+    // after rearranging the chars of old string 
 
     // Logic : using sliding window 
-    // store string s1 chars count 
-    // traverse s2 using window of size s1 
-    // store s2 chars count 
-    // check if s1 chars count is same as s2 chars count 
+    // first traverse the string s1 
+    // and store thw frequency of chars of s1 in s1_char_freq[] 
+    // then traverse the string s2 from i = 0 to i < s1.length and i < s2.length 
+    // and store thw frequency of chars of s2 in s2_char_freq[] 
+    // now check if both s1_char_freq[] and s2_char_freq[] are same 
+    // if same return true as string s2 has a permutation of string s1 
+    // else move to next elements of s2 if exists 
+    // run a loop till i < s2.length 
+    // increament freq of ith char of string s2 
+    // and decreament freq of the element that is not visible in window i.e. i - s1.length 
+    // now check if both array of frequency has same freq of elements 
+    // if yes then return true 
+    // else increament i by 1 and move to next element 
 
     string s1 = "ab", s2 = "eidbaooo";
 
-    int s1_count[26] = {0};
+    // char array to store frequency of chars of string s1 
+    int s1_char_freq[26] = {0};
 
+    // traversing s1 
     for (int j = 0; j < s1.length(); j++) {
+        // storing freq 
         int index = s1[j] - 'a';
-        s1_count[index]++;
+        s1_char_freq[index]++;
     }
 
     // traversing s2 
     int i = 0;
     int window_size = s1.length();
-    int s2_count[26] = {0};
+    // char array to store frequency of chars of string s2 
+    int s2_char_freq[26] = {0};
 
-    // first window 
+    // for first window 
     while (i < window_size && i < s2.length()) {
+        // storing freq 
         int index = s2[i]-'a';
-        s2_count[index]++;
+        s2_char_freq[index]++;
         i++;
     }
 
-    // checkEquality 
-    if (checkEquality(s1_count, s2_count) == 1) {
+    // checking equality of freq 
+    if (checkEquality(s1_char_freq, s2_char_freq) == 1) {
         cout << "true" << endl;
         return;
     }
 
-    // all windows 
+    // for all other windows 
     while (i < s2.length()) {
-        // new char count 
+        // new char freq 
         int index = s2[i]-'a';
-        s2_count[index]++;
+        s2_char_freq[index]++;
 
-        // old char count 
+        // old char freq 
         index = s2[i-window_size]-'a';
-        s2_count[index]--;
+        s2_char_freq[index]--;
 
-        i++;
-
-        // checkEquality
-        if (checkEquality(s1_count, s2_count) == 1) {
+        // checkEquality 
+        if (checkEquality(s1_char_freq, s2_char_freq) == 1) {
             cout << "true" << endl;
             return;
         }
+
+        i++;
     }
 
     cout << "false" << endl;
 }
 
 void p11() {
-    // Problem 10 : 1047. Remove All Adjacent Duplicates In String - https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/ 
+    // Problem 10 : LC 1047. Remove All Adjacent Duplicates In String - https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/ 
 
     // Statement : You are given a string s consisting of lowercase English letters. 
     // A duplicate removal consists of choosing two adjacent and equal letters and removing them.
@@ -337,34 +424,36 @@ void p11() {
     // Return the final string after all such duplicate removals have been made. 
     // It can be proven that the answer is unique.
 
+    // Logic : using stack 
+    // initialise a stack st 
+    // traverse the string str from i = 0 to i < str.length 
+    // if stack is empty or top element of stack is not equal to str[i]
+    // push the str[i] into stack 
+    // else pop the last element from stack 
+    // increament i by 1 and move to next element 
+    // lastly make a new string using the stack 
+    // then reverse the final string 
+
     string str = "abbaca"; // output : ca 
-
-    // -------------- Not Optimised ---------------- // 
-    // int i = 0;
-    // while (i < str.length()) {
-    //     if (str[i] == str[i+1]) {
-    //         str.erase(i, 2);
-    //         i = 0;
-    //         continue;
-    //     }
-    //     i++;
-    // }
-    // -------------- Not Optimised ---------------- // 
-
-    string final_str = "";
+    string final_str;
+    stack<char> st;
 
     for (int i = 0; i < str.length(); i++) {
-        if (final_str.back() != str[i]) {
-            final_str.push_back(str[i]);
+       if (st.empty() || st.top() != str[i]) {
+            st.push(str[i]);
         } else {
-            final_str.pop_back();
+            st.pop();
         }
     }
 
-    cout << final_str;
+    while (st.size() > 0) {
+        final_str.push_back(st.top());
+        st.pop();
+    }
 
-    // !!!!!!!!!!!!!!!!!! not done !!!!!!!!!!!!!!!!!!!!!!!!!!! // 
-    // !!!!!!!!!!!!!!!!!! homework !!!!!!!!!!!!!!!!!!!!!!!!!!! // 
+    reverse(final_str.begin(), final_str.end());
+
+    cout << final_str;
 }
 
 void p12() {
@@ -376,11 +465,24 @@ void p12() {
     // For each group of consecutive repeating characters in chars:
     // If the group's length is 1, append the character to s.
     // Otherwise, append the character followed by the group's length.
-    // The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+    // The compressed string s should not be returned separately, but instead, be stored in the input character array chars. 
+    // Note that group lengths that are 10 or longer will be split into multiple characters in chars.
     // After you are done modifying the input array, return the new length of the array.
     // You must write an algorithm that uses only constant extra space.
 
-    char chars[8] = {'a','a','b','b','c','c','c'};;
+    // Logic : using two pointer approach 
+    // take a variable count = 0 
+    // traverse the chars array from s = 0 to s < size of chars array 
+    // inside run a loop from j = s+1 to j < size  
+    // and find the count of all the consecutive elements (==chars[s])
+    // then store the char chars[s] in chars at count index 
+    // then increament count by 1 
+    // now store the occurrence of chars[s] in chars at count index 
+    // go to next different element in chars 
+    // lastly remove all the extra chars from chars array 
+    // from i = count to i < size 
+
+    char chars[8] = {'a','a','b','b','c','c','c'};
     int size = 7;
 
     int count = 0;
