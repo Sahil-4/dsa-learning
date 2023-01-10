f8e9facf15417e2d8a0f9965e3d8fe961987f1e4
Sahil-4 <www.sahil.875020@gmail.com>
2023-01-10 18:08:10 +0530

    updated code for lecture 33 added homework

--- lecture033.py
+++ lecture033.py
@@ -226,6 +226,387 @@ def p5():
         print("key not found")
 
 
+def findOccurrences(arr, key, index, low, high, fl):
+    # for p5()
+    if low > high:
+        return index
+
+    mid = low + int((high-low)/2)
+
+    if arr[mid] == key:
+        index = mid
+        if fl == 0:
+            # 0 for first occurrence
+            high = mid-1
+        elif fl == 1:
+            # 1 for last occurrence
+            low = mid+1
+    elif arr[mid] > key:
+        high = mid-1
+    else:
+        low = mid+1
+
+    return findOccurrences(arr, key, index, low, high, fl)
+
+
+def p6():
+    # Problem 6 : Find first and last occurrence - https://www.codingninjas.com/codestudio/problems/first-and-last-position-of-an-element-in-sorted-array_1082549
+
+    # Stattement : You have been given a sorted array/list ARR consisting of ‘N’ elements,
+    # and an integer ‘K’,
+    # the task is to find the first and last occurrence of ‘K’ in ARR
+
+    # Logic : using Recursive Binary Search
+    # find the first occurrence of element k in the array
+    # find the last occurrence of element k in the array
+    # create a recursive function findOccurrences()
+    # which will take an array, key, answer = -1, low index, high index, flag = 0 or flag = 1
+    # inside function add a base condition
+    # if low > high return answer
+    # calculate mid = low + (high-low)/2
+    # check for below 3 conditions
+    # 1. arr[mid] == key
+    # 2. arr[mid] > key
+    # 3. arr[mid] < key
+    # if 1 is true update answer = mid
+    # and then check value flag
+    # if flag == 0
+    # update high = mid-1
+    # if flag == 1
+    # update low = mid+1
+    # if 2 is true update high = mid-1
+    # if 3 is true update low = mid+1
+    # then finally return recursive call to findOccurrences()
+
+    # Code :
+    arr = [0, 2, 2, 5, 5, 12, 12, 12, 14, 17, 18, 18]
+    key = 12
+
+    arr = [0, 2, 2, 5, 5, 12, 12, 12, 14, 17, 18]
+    key = 12
+
+    arr = [12, 12, 12, 12, 12, 12]
+    key = 12
+
+    occurrence_first = findOccurrences(arr, key, -1, 0, len(arr)-1, 0)
+    occurrences_last = findOccurrences(arr, key, -1, 0, len(arr)-1, 1)
+
+    # first and last occurrence
+    print(occurrence_first, occurrences_last)
+
+    # total occurrences
+    print(occurrences_last-occurrence_first)
+
+
+def findPeak(arr, low, high):
+    # for p7()
+    if low > high:
+        return -1
+
+    mid = low + int((high-low)/2)
+
+    if arr[mid-1] < arr[mid] > arr[mid+1]:
+        return mid
+    elif arr[mid] > arr[mid-1]:
+        low = mid+1
+    else:
+        high = mid
+
+    return findPeak(arr, low, high)
+
+
+def p7():
+    # Problem 7 : 852. Peak Index in a Mountain Array - https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+    # Statement : Given a mountain array arr,
+    # return the index i
+    # such that arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+    # Logic : using recursive binary search
+    # create a recursive function findPeak(arr[], low_index, high_index)
+    # add base condition
+    # if low_index > high_index return -1
+    # check for below condtions
+    # 1. arr[mid-1] < arr[mid] > arr[mid+1]
+    # 2. arr[mid] > arr[mid-1]
+    # 3. arr[mid] < arr[mid-1]
+    # if 1 is true simply return mid as peak index
+    # if 2 is true update low = mid+1
+    # if 3 is true update high = mid
+    # then finally return a recursive call findPeak(arr[], low_index, high_index)
+
+    # Code :
+    arr = [24, 69, 100, 99, 79, 78, 67, 36, 26, 19]
+
+    peak = findPeak(arr, 0, len(arr)-1)
+
+    print(peak)
+
+
+def sum(arr, l, h):
+    # for findPivot(arr, low, high)
+    sum = 0
+    for i in range(l, h):
+        sum += arr[i]
+
+    return sum
+
+
+def findPivot(arr, low, high):
+    # for p8()
+    if low > high:
+        return -1
+
+    mid = low + int((high-low)/2)
+
+    l_sum = sum(arr, 0, mid)
+    r_sum = sum(arr, mid+1, len(arr))
+
+    if l_sum == r_sum:
+        return mid
+    elif l_sum > r_sum:
+        high = mid-1
+    else:
+        low = mid+1
+
+    return findPivot(arr, low, high)
+
+
+def p8():
+    # Problem 8 : 724. Find Pivot Index - https://leetcode.com/problems/find-pivot-index/
+
+    # Statement : Given an array of integers nums[],
+    # calculate the pivot index of this array.
+    # The pivot index is the index where the sum of all the numbers,
+    # strictly to the left of the index is
+    # equal to the sum of all the numbers,
+    # strictly to the index's right.
+    # NOTE : If the index is on the left edge of the array,
+    # then the left sum is 0 because there are no elements to the left.
+    # This also applies to the right edge of the array.
+
+    # Logic :
+    # take 2 variables right_sum = 0, left_sum = 0
+    # loop through the array
+    # calculate right_sum summing all the elements
+    # run loop from i = 0 to i < size
+    # add arr[i-1] in left_sum,
+    # and subtract arr[i] from right_sum
+    # check if right_sum == left_sum,
+    # if yes return i as pivot index
+    # else move to next index
+    # NOTE : will only work if array contains only positive integers
+
+    arr = [1, 7, 3, 6, 5, 6]
+    # arr = [1, 2, 3]
+    # arr = [1, 7, 3, 6, 5, 6]
+
+    print(findPivot(arr, 0, len(arr)-1))
+
+
+def pivot(arr, low, high):
+    # for p8()
+    if low >= high:
+        return low
+
+    mid = low + int((high - low)/2)
+    if arr[mid] >= arr[0]:
+        low = mid+1
+    else:
+        high = mid
+
+    return pivot(arr, low, high)
+
+
+def p9():
+    # Problem 9 : Find pivot in an sorted and rotated array
+
+    # Statement : given a sorted and rotated array of integers
+    # return the pivot (minimum) element of the array
+
+    # Logic : Using binary search
+    # take 3 vars low = 0, high = size-1, mid = mid = (low+high)/2
+    # create a recursive function pivot()
+    # which will take an array low index and high index
+    # add base condition
+    # if l >= h return low as answer
+    # calculate mid = (low+high)/2
+    # check for below conditions
+    # 1. arr[mid] >= arr[0]
+    # 2. arr[mid] < arr[0]
+    # if 1 is true update low = mid+1
+    # if 2 is true update high = mid
+    # after that call the pivot() function again
+
+    # Code :
+    # arr = [5, 6, 7, 7, 9, 1, 2, 3, 4]
+    # arr = [5, 7, 7, 9, 1]
+    # arr = [13, 14, 15, 16, 17, 18, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    arr = [13, 14, 15, 168, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    p = pivot(arr, 0, len(arr)-1)
+
+    print(p)
+
+    pass
+
+
+def findRoot(n, ans, low, high):
+    if low > high:
+        return ans
+
+    mid = low + int((high-low)/2)
+    sq = mid*mid
+    if sq == n:
+        return mid
+    elif sq < n:
+        ans = mid
+        low = mid+1
+    else:
+        high = mid-1
+
+    return findRoot(n, ans, low, high)
+
+
+def p10():
+    # Problem 10 : find square root of n
+
+    # Statement : given an integer n,
+    # return square root of n
+
+    # Logic : using recursive binary search
+    # low = 0, high = n
+    # create a recursive function findRoot(),
+    # which will take an integer n, answer ans, low range and high range
+    # and will return the square root of n
+    # add base condition
+    # if low > high return ans
+    # find mid and check below conditions
+    # 1. mid*mid == n;
+    # 2. mid*mid < n;
+    # 3. mid*mid > n;
+    # if 1 is true return mid as answer
+    # if 2 is true save mid as answer and update low = mid+1
+    # if 3 is true update high = mid-1
+    # after that call the findRoot() function again with all the arguments
+
+    # Code :
+    n = 43
+    r = findRoot(n, -1, 0, n)
+
+    print(r)
+
+    pass
+
+
+def allocate(arr, m, mid):
+    students = 1
+    pages = 0
+
+    for i in range(len(arr)):
+        if arr[i] > mid:
+            return False
+
+        if pages + arr[i] <= mid:
+            pages += arr[i]
+        else:
+            students += 1
+            pages = arr[i]
+
+    if students > m:
+        return False
+    else:
+        return True
+
+
+def solve_bookallocation(arr, m, n, ans, low, high):
+    if low > high:
+        return ans
+
+    mid = low + int((high-low)/2)
+
+    # if mid is a answer
+    if allocate(arr, m, mid):
+        ans = mid
+        high = mid-1
+    else:
+        low = mid+1
+
+    return solve_bookallocation(arr, m, n, ans, low, high)
+
+
+def p11():
+    # Problem 11 : Book allocation problem - https://www.codingninjas.com/codestudio/problems/allocate-books_1090540
+
+    # Statement : given an array of n integers,
+    # representing n number of book's pages,
+    # and an integer m denoting number of students
+    # the task is to allocate books in m students such that
+    # 1. all books should be allocated
+    # 2. every student should have atleast one book
+    # 3. book allocation should be in contiguous manner
+    # books should be allocated such that maximum number of pages,
+    # allocated to a student should be minimum
+
+    # Logic : using recursive Binary Search
+    # first find the search space
+    # in worst case low can be maximum number of pages of books,
+    # and high can be sum of all pages
+    # create a function solve_bookallocation(),
+    # which will take array of books,
+    # number of total students
+    # answer ans (initially ans = -1)
+    # low index and high index
+    # add base condition
+    # if low > high return ans
+    # find mid and check if it can be a solution or not
+    # if it is a solution save it as ans = mid and update high = mid-1
+    # else update low = mid+1
+    # to check wether mid is a solution or not
+    # create a function allocate(),
+    # which will take array of books pages
+    # number of students
+    # allocate mid number of pages to m students
+    # initialise students = 1 
+    # allocated_pages = 0
+    # traverse the array of pages count of books from i = 0 i < size_of_array 
+    # check if arr[i] > mid
+    # if true return false 
+    # check if allocated_pages + arr[i] < mid
+    # if true then add arr[i] in allocated_pages 
+    # else update students count by 1 
+    # and pages = arr[i]
+    # after this check if total count of students is bigger than the given number of students 
+    # if true return false 
+    # else return true 
+
+    # Code :
+    # arr = [21, 22, 40, 12, 45, 60]
+    # arr = [5, 17, 100, 11]
+    # m = 4
+    # arr = [12, 34, 67, 90]
+    # m = 2
+    # arr = [25, 46, 28, 49, 24]
+    # m = 4
+    arr = [10, 10, 20]
+    m = 2
+
+    n = len(arr)
+
+    ans = -1
+
+    low = 0
+    high = 0
+    for elements in arr:
+        if elements > low:
+            low = elements
+        high += elements
+
+    ans = solve_bookallocation(arr, m, n, ans, low, high)
+
+    print(ans)
+    pass
+
+
 if __name__ == "__main__":
     # Lecture 33 : Recursion III Binary search
 
@@ -238,5 +619,17 @@ if __name__ == "__main__":
     # p4()
 
     # p5()
+
+    # p6()
+
+    # p7()
+
+    # p8()
+
+    # p9()
+
+    # p10()
+
+    # p11()
 
     pass
